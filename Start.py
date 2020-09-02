5820cfaccbbbc2d7f3d734b87577165901883b5e
kaobeosolu78 <osolukao@msu.edu>
2020-09-02 15:36:35 -0400

    refactored Course methods add_pages and calculate_urgency

--- Start.py
+++ Start.py
@@ -25,10 +25,13 @@ class Chapter:
     def __init__(self, chapter, chapter_info):
         self.chapter_name = chapter
         self.exam_number, self.start_page, self.end_page = chapter_info
+        self.urgency = 0
 
     def get_page_sum(self):
         return self.end_page-self.start_page
 
+    def calculate_urgency(self, exam_page_sum, date_range_length):
+        self.urgency = round((self.get_page_sum()/exam_page_sum)*date_range_length)
 
 class Course:
     def __init__(self, name, start_date, exam_dates, chapters, cooldown):
@@ -44,12 +47,13 @@ class Course:
         self.all_dates = date_range(self.exam_dates[0], self.exam_dates[-1])
         self.calendar = {}
 
+        self.finalize()
         # call necessary methods
         self.get_exam_date_ranges()
         self.adjust_dates()
         self.total_pages_to_study()
         self.add_pages()
-        # self.calc_urgency()
+        self.calculate_urgency()
         # self.set_dates()
         # self.finalize()
         # self.display()
@@ -58,18 +62,18 @@ class Course:
     # creates a list of datetimes between each exam
     def get_exam_date_ranges(self):
         for exam_date_ind, exam in enumerate(self.exam_info):
-            exam["Date Range"].append((date_range(([self.start_date]+self.exam_dates)[exam_date_ind],
-                                                  ([self.start_date]+self.exam_dates)[exam_date_ind+1])))
+            exam["Date Range"] = date_range(([self.start_date] + self.exam_dates)[exam_date_ind],
+                                                  ([self.start_date] + self.exam_dates)[exam_date_ind+1])
 
     # removes the exam date from all date ranges and a variable number of dates after the exam
-    def adjust_dates(self):
+    def adjust_dates(self):  # improve
         for remove_date in [exam_date+datetime.timedelta(days=days_after_exam)
-                      for exam_date in self.exam_dates[1:] for days_after_exam in range(3)]:
+                for exam_date in self.exam_dates[1:] for days_after_exam in range(3)]:
             if remove_date in self.all_dates:
                 del self.all_dates[self.all_dates.index(remove_date)]
             for exam in self.exam_info:
                 if remove_date in exam["Date Range"]:
-                    del exam["Date Range"][self.exam_date_ranges[ind].index(remove_date)]
+                    del exam["Date Range"][exam["Date Range"].index(remove_date)]
 
     def total_pages_to_study(self):
         self.total_page_num = sum([chapter_obj.get_page_sum() for chapter_obj in list(self.chapter_info.values())])
@@ -83,24 +87,34 @@ class Course:
 
     # for each chapter, create a ratio [(pages in chapter/pages required for exam)*(total dates)] that will be used to
     # calculate how many days each chapter should be studied
-    def calc_urgency(self):
+    def calculate_urgency(self):
+        for exam_number, exam in enumerate(self.exam_info):
+            test = 0
+            for chapter in list(self.chapter_info.values()):
+                if chapter.exam_number == exam_number:
+                    chapter.calculate_urgency(exam["Total Pages"], len(exam["Date Range"]))
+                    test += chapter.urgency
+            #         temp = len(exam["Date Range"])
+            # print(test, temp) May have to add edge case
 
 
-
-        test, temp = 0, ""
-        for ind in range(len(self.exam_date_ranges)):
-            for chap in list(self.chapter_info.keys()):
-                # calculate urgency ratio
-                if self.chapter_info[chap][0] == ind:
-                    temp = chap
-                    self.chapter_info[chap].append(("Urgency", round((self.chapter_info[chap][3][1] / self.exam_info[ind][0][1]) * len(self.exam_date_ranges[ind]))))
-                    test += round((self.chapter_info[chap][3][1] / self.exam_info[ind][0][1]) * len(self.exam_date_ranges[ind]))
-            # edge case, rounds differently if on the last date of the range
-            if test > len(self.exam_date_ranges[ind]):
-                self.chapter_info[temp][4] = ("Urgency",int((self.chapter_info[temp][3][1] / self.exam_info[ind][0][1]) * len(self.exam_date_ranges[ind])))
+        # test, temp = 0, ""
+        # for ind in range(len(self.exam_date_ranges)):
+        #     for chap in list(self.chapter_info.keys()):
+        #         # calculate urgency ratio
+        #         if self.chapter_info[chap][0] == ind:
+        #             temp = chap
+        #             self.chapter_info[chap].append(("Urgency", round((self.chapter_info[chap][3][1] / self.exam_info[ind][0][1]) * len(self.exam_date_ranges[ind]))))
+        #             test += round((self.chapter_info[chap][3][1] / self.exam_info[ind][0][1]) * len(self.exam_date_ranges[ind]))
+        #     # edge case, rounds differently if on the last date of the range
+        #     if test > len(self.exam_date_ranges[ind]):
+        #         self.chapter_info[temp][4] = ("Urgency",int((self.chapter_info[temp][3][1] / self.exam_info[ind][0][1]) * len(self.exam_date_ranges[ind])))
 
     # create a calendar and portion out the chapters
     def set_dates(self):
+        # calendar = {Class_Name: {Exam_Num:{Chapter_Name:[List_of_Study_Dates]}}
+
+
         temp = self.all_dates
         count = 0
         chaps = list(self.chapter_info.keys())
@@ -121,8 +135,9 @@ class Course:
     # pickle out calendar
     def finalize(self):
         calendars = load_obj("calendars")
-        calendars[self.name] = self.calendar
-        save_obj("calendars", calendars)
+        print("")
+        # calendars[self.name] = self.calendar
+        # save_obj("calendars", calendars)
 
 
 def main():
